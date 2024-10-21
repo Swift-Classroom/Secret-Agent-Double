e9026aab80d7addddc26b2ad27cb9657e11f799b
Gianluca Orpello <g.orpello@gmail.com>
2024-10-21 16:25:39 +0200

    Switch to Swift Testing

--- Tests/SecretAgentTests/SecretAgentTests.swift
+++ Tests/SecretAgentTests/SecretAgentTests.swift
@@ -1,43 +1,32 @@
-import XCTest
+import Testing
 
-@testable import SecretAgent
-
-final class SecretAgentTests: XCTestCase {
-  let protected = protectSecret(
-    "UMBRA will fill everyone's sugar bowls with salt!", withPassword: "P455w0rd")
-
-  func testPasswordSuccess() {
-    XCTAssertEqual(protected("P455w0rd"), "UMBRA will fill everyone's sugar bowls with salt!")
-  }
-
-  func testPasswordFail() throws {
+struct SecretAgentTests {
+    let protected = protectSecret(
+        "UMBRA will fill everyone's sugar bowls with salt!", withPassword: "P455w0rd")
     
-    XCTAssertEqual(protected("hunter2"), "Sorry. No hidden secrets here.")
-  }
-
-  func testCombination1() throws {
+    @Test func testPasswordSuccess() async throws {
+        #expect(protected("P455w0rd") == "UMBRA will fill everyone's sugar bowls with salt!")
+    }
     
-    // This is an example of a functional test case.
-    // Use XCTAssert and related functions to verify your tests produce the correct
-    // results.
-    let combo = generateCombination(forRoom: 1, usingFunction: { ($0 * 127 + 19) % 256 })
-    XCTAssertTrue(combo == (146, 129, 18))
-  }
-
-  func testCombination2() throws {
+    @Test func testPasswordFail() async throws {
+        #expect(protected("hunter2") == "Sorry. No hidden secrets here.")
+    }
     
-    let combo = generateCombination(
-      forRoom: 1,
-      usingFunction: {
-        ($0 * 73 + 161) % 256
-      })
-    XCTAssertTrue(combo == (234, 91, 148))
-  }
-
-  static var allTests = [
-    ("testPasswordSuccess", testPasswordSuccess),
-    ("testPasswordFail", testPasswordFail),
-    ("testCombination1", testCombination1),
-    ("testCombination2", testCombination2),
-  ]
+    @Test func testCombination1() async throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        let combo = generateCombination(forRoom: 1, usingFunction: { ($0 * 127 + 19) % 256 })
+        #expect(combo == (146, 129, 18))
+    }
+    
+    @Test func testCombination2() async throws {
+        let combo = generateCombination(
+            forRoom: 1,
+            usingFunction: {
+                ($0 * 73 + 161) % 256
+            })
+        #expect(combo == (234, 91, 148))
+    }
+    
 }
